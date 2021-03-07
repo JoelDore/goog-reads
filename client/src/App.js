29fc6ee57ea8ee058d533a704e575baa919c20f9
JES20 <dore.joel.dore@gmail.com>
2021-03-07 15:14:09 -0500

    import Saved/Search pages into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import Saved from './pages/Saved'
+import Search from './pages/Search'
 
 function App() {
   return (
     <div className="App">
-
+      <Search />
+      <Saved />
     </div>
   );
 }
