a920b2e04dc469d535d8925bcb7f3183e1525070
JES20 <dore.joel.dore@gmail.com>
2021-03-07 16:08:22 -0500

    Style Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,31 +4,32 @@ import { Link, useLocation } from 'react-router-dom'
 import BootNavbar from 'react-bootstrap/Navbar'
 import BootNav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import './nav.css'
 
 export default function Nav() {
     const location = useLocation()
 
     return (
-        <Container fluid>
-            <BootNavbar expand="md">
-                <BootNavbar.Brand href="#home">googreads</BootNavbar.Brand>
-                <BootNavbar.Toggle aria-controls="main-nav" />
+        <Container fluid className="bg-googreads">
+            <BootNavbar expand="md" variant="dark">
+                <BootNavbar.Brand>googreads</BootNavbar.Brand>
+                <BootNavbar.Toggle aria-controls="main-nav" className="border-0" />
                 <BootNavbar.Collapse id="main-nav">
                     <BootNav className="ms-auto">
                         <Link
                             to="/search"
-                            className={
-                                `${location.pathname === "/" || location.pathname === "/search"
-                                    ? "nav-link active"
-                                    : "nav-link"}`
-                            }
+                            className={`${['/', '/search'].includes(location.pathname)
+                                ? "nav-link active"
+                                : "nav-link"}`}
                         >
                             Search
                         </Link>
 
                         <Link
                             to="/saved"
-                            className={`${location.pathname === "/saved" ? "nav-link active" : "nav-link"}`}
+                            className={`${location.pathname === "/saved"
+                                ? "nav-link active"
+                                : "nav-link"}`}
                         >
                             Saved
                         </Link>
