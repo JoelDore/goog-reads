49facdad21e6a0b39307a97892064926f1d1a108
JES20 <dore.joel.dore@gmail.com>
2021-03-07 16:28:17 -0500

    Fix API bug

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ import axios from 'axios'
 
 // Gets all books from DB
 const getBooks = () => {
-    axios.get('/api/books')
+    return axios.get('/api/books')
 }
 // Saves a book to DB
 const saveBook = (book) => {
-    axios.post('/api/books', book)
+    return axios.post('/api/books', book)
 }
 // Deletes a book by ID
 const deleteBook = (id) => {
-    axios.get(`/api/books/${id}`)
+    return axios.get(`/api/books/${id}`)
 }
 // Gets books from Google Books by search query
 const getGoogleBooks = (query, startIndex) => {
-    axios.get(`/api/googlebooks?q=${query}&startIndex=${startIndex}`)
+    return axios.get(`/api/googlebooks?q=${query}&startIndex=${startIndex}`)
 }
 
 export default {
