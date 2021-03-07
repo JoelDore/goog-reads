0571cd04f18fb5c6047ed13b0d4d8f3297a15568
JES20 <dore.joel.dore@gmail.com>
2021-03-07 16:29:04 -0500

    Create method to search for books

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import API from '../utils/API'
 
 export default function Search() {
+    const inputRef = useRef()
+
+    const [books, setBooks] = useState([])
+    const [currIndex, setCurrIndex] = useState(0)
+    // const [query, setQuery] = useState([])
+
+    const searchBooks = async () => {
+        const query = inputRef.current.value
+        const results = await API.getGoogleBooks(query, currIndex)
+        setBooks(results.data.items)
+        inputRef.current.value = ""
+    }
+
     return (
         <div>
-
+            <h1>Search</h1>
+            <input type="text" name="query" ref={inputRef} />
+            <button type="button" onClick={searchBooks}>Search books</button>
+            {
+                books && books.map((book, idx) => (
+                    <p key={book.id}>{currIndex + 1 + idx}: {book.volumeInfo.title}</p>
+                ))
+            }
         </div>
     )
 }
