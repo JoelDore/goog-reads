d9c42b205f9dc42e3086d27ee176652b382dcc2f
JES20 <dore.joel.dore@gmail.com>
2021-03-08 09:12:13 -0500

    Add logo to Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,14 +5,17 @@ import BootNavbar from 'react-bootstrap/Navbar'
 import BootNav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import './nav.css'
-
+import logo from '../../assets/images/googreads-logo.svg'
 export default function Nav() {
     const location = useLocation()
 
     return (
         <Container fluid className="bg-googreads">
-            <BootNavbar expand="md" variant="dark">
-                <BootNavbar.Brand>goog<b>reads</b></BootNavbar.Brand>
+            <BootNavbar expand="md" variant="dark" className="px-0">
+                <BootNavbar.Brand>
+                    <img src={logo} alt="googreads logo" width={32} height={32} className="mr-2 ml-0" />
+                    <span>goog<b>reads</b></span>
+                </BootNavbar.Brand>
                 <BootNavbar.Toggle aria-controls="main-nav" className="border-0" />
                 <BootNavbar.Collapse id="main-nav">
                     <BootNav className="mr-auto ml-3">
