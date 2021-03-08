59eb52a15f1d8f0b1bf1d47fda2a264cff7e6889
JES20 <dore.joel.dore@gmail.com>
2021-03-08 08:24:20 -0500

    Add pagination to book search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,24 +6,50 @@ export default function Search() {
 
     const [books, setBooks] = useState([])
     const [currIndex, setCurrIndex] = useState(0)
+    const [currQuery, setCurrQuery] = useState("")
 
-    const searchBooks = async () => {
-        const query = inputRef.current.value
-        const results = await API.getGoogleBooks(query, currIndex)
+    const getBooks = async (query, startIndex) => {
+        const results = await API.getGoogleBooks(query, startIndex)
         setBooks(results.data.items)
-        inputRef.current.value = ""
+    }
+
+    const handleSearch = async () => {
+        // Get query from input & validate
+        const query = inputRef.current.value
+        if (!query) return
+
+        // Get books & set current query
+        await getBooks(query, 0)
+        setCurrQuery(query)
+        setCurrIndex(0)
+    }
+
+    const nextPage = async () => {
+        const nextIndex = currIndex + 10
+        // Get next page of books & update index
+        await getBooks(currQuery, nextIndex)
+        setCurrIndex(nextIndex)
+    }
+
+    const prevPage = async () => {
+        const prevIndex = currIndex - 10
+        // Get prev page of books & update index
+        await getBooks(currQuery, prevIndex)
+        setCurrIndex(prevIndex)
     }
 
     return (
         <div>
             <h1>Search</h1>
             <input type="text" name="query" ref={inputRef} />
-            <button type="button" onClick={searchBooks}>Search books</button>
+            <button type="button" onClick={handleSearch}>Search books</button>
             {
-                books && books.map((book, idx) => (
-                    <p key={book.id}>{currIndex + 1 + idx}: {book.volumeInfo.title}</p>
-                ))
+                books.length ? books.map((book, idx) => (
+                    <p key={book.id}>{currIndex + idx + 1}: {book.volumeInfo.title}</p>
+                )) : <h3>No results found</h3>
             }
+            {currIndex !== 0 && <button type="button" onClick={prevPage}>Prev Page</button>}
+            <button type="button" onClick={nextPage}>Next Page</button>
         </div>
     )
 }
