fcfb4442e8c96b1ff0e4446776d097131f18c45d
JES20 <dore.joel.dore@gmail.com>
2021-03-08 11:35:54 -0500

    Adjust Jumbotron small text position

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ export default function Jumbotron() {
         <BootJumbotron className="text-center position-relative">
             <h1 style={{ fontWeight: "300" }}>Welcome to goog<b>reads</b>!</h1>
             <p>Search for and save your favorite books</p>
-            <small style={{ position: "absolute", bottom: 10, right: 10 }}>Powered by Google Books</small>
+            <small style={{ position: "absolute", bottom: 5, right: 10 }}>Powered by Google Books</small>
         </BootJumbotron>
     )
 }
