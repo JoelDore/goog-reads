813b86fa9ad57e0cf8c24ece1b0db853d1b448e2
JES20 <dore.joel.dore@gmail.com>
2021-03-08 09:02:34 -0500

    Create Jumbotron component

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Jumbotron as BootJumbotron } from 'react-bootstrap'
+
+export default function Jumbotron() {
+    return (
+        <BootJumbotron className="text-center position-relative">
+            <h1 style={{ fontWeight: "300" }}>Welcome to goog<b>reads</b>!</h1>
+            <p>Search for and save your favorite books</p>
+            <small style={{ position: "absolute", bottom: 10, right: 10 }}>Powered by Google Books</small>
+        </BootJumbotron>
+    )
+}
